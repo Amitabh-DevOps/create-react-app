f14501bcafb33c766590c4fb592bdfcf1f374979
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-18 12:56:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull amitabhdevops/react-app:v1
 # Run the Docker image as a container
-docker run -d -p 80:80 docker pull amitabhdevops/react-app:v1
+docker run -d -p 80:80 amitabhdevops/react-app:v1
