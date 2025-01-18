a8986b899bd68508a94d53cf3a1e7725da569203
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-18 14:37:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from "react-dom"; // Import ReactDOM to render the app
+import "./index.css"; // Import global CSS styles
+import App from "./App"; // Import the main App component
 
+// Rendering the App component inside the root div in index.html
 ReactDOM.render(
   <React.StrictMode>
     <App />
