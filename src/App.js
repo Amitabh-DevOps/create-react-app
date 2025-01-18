4b171271b4a398c8a353280b291acc3220b8886d
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-18 14:37:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from './logo.svg'; // Make sure to import the logo correctly
+import './App.css'; // Importing the CSS file
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="React Logo" />
+        <h1>Welcome to React!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
