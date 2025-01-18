0454e40874e5bad0d569fb2308a8faee64dd1f45
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-18 14:44:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-# Check if the Docker image exists locally
-if ! docker images | grep -q "amitabhdevops/react-app.*v1"; then
-  echo "Image not found locally. Pulling from Docker Hub..."
-  docker pull amitabhdevops/react-app:v1
-else
-  echo "Image already exists locally. Skipping pull."
-fi
+# pulling docker images
+docker pull amitabhdevops/react-app:v1
 
 # Run the Docker image as a container
 docker run -d -p 80:80 amitabhdevops/react-app:v1
