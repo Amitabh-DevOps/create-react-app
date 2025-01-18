c5ccd608a6be5824d4c235879dc25ed63a1d0606
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-18 13:10:27 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,7 +4,7 @@ set -e
 # Stop the running container (if any)
 containerid=$(docker ps -q)
 if [ -n "$containerid" ]; then
-  docker rm -f $containerid
+  docker stop $containerid && docker rm -f $containerid
 else
   echo "No running containers to remove."
 fi
